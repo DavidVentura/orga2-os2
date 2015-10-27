bbae70b3fbd8fb0b2d682c8ed444d2a35e154af6
Fabian <falvarez.0104@gmail.com>
2015-10-27 18:43:52 -0300

    Ahora la tabla de la idt es más linda

--- idt.c
+++ idt.c
@@ -36,35 +36,36 @@ idt_descriptor IDT_DESC = {
 
 // descomentar para usar, seteando segsel y attr segun corresponda
 
-#define IDT_ENTRY(numero, dpl)                                                                                   \
-    idt[numero].offset_0_15 = (unsigned short) ((unsigned int)(&_isr ## numero) & (unsigned int) 0xFFFF);        \
-    idt[numero].segsel = (unsigned short) 0x08 << 3;                                                                  \
-    idt[numero].attr = (unsigned short) 0b1000111000000000 | (((unsigned short)(dpl & 0x3)) << 13);             \
-    idt[numero].offset_16_31 = (unsigned short) ((unsigned int)(&_isr ## numero) >> 16 & (unsigned int) 0xFFFF);
 
-    /*idt[numero].attr = (unsigned short) 0x8E00 | (((unsigned short)(dpl & 0x3)) << 13);             \*/
+#define IDT_ENTRY(numero, _dpl, type)                                                                        \
+    idt[numero].offset_0_15 = (unsigned short) ((unsigned int)(&_isr ## numero) & (unsigned int) 0xFFFF);   \
+    idt[numero].segsel = (unsigned short) 0x08 ;                                                            \
+    idt[numero].tipo = type;                                                                           \
+    idt[numero].dpl = _dpl;                                                                                  \
+    idt[numero].p = 1;                                                                                      \
+    idt[numero].offset_16_31 = (unsigned short) ((unsigned int)(&_isr ## numero) >> 16 & (unsigned int) 0xFFFF);
 
 void idt_inicializar() {
     // Excepciones
-	IDT_ENTRY(0, 0);
-	IDT_ENTRY(1, 0);
-	IDT_ENTRY(2, 0);
-	IDT_ENTRY(3, 0);
-	IDT_ENTRY(4, 0);
-	IDT_ENTRY(5, 0);
-	IDT_ENTRY(6, 0);
-	IDT_ENTRY(7, 0);
-	IDT_ENTRY(8, 0);
-	IDT_ENTRY(9, 0);
-	IDT_ENTRY(10, 0);
-	IDT_ENTRY(11, 0);
-	IDT_ENTRY(12, 0);
-	IDT_ENTRY(13, 0);
-	IDT_ENTRY(14, 0);
-	IDT_ENTRY(15, 0);
-	IDT_ENTRY(16, 0);
-	IDT_ENTRY(17, 0);
-	IDT_ENTRY(18, 0);
-	IDT_ENTRY(19, 0);
+	IDT_ENTRY(0, 0, idt_entry_int);
+	IDT_ENTRY(1, 0, idt_entry_int);
+	IDT_ENTRY(2, 0, idt_entry_int);
+	IDT_ENTRY(3, 0, idt_entry_int);
+	IDT_ENTRY(4, 0, idt_entry_int);
+	IDT_ENTRY(5, 0, idt_entry_int);
+	IDT_ENTRY(6, 0, idt_entry_int);
+	IDT_ENTRY(7, 0, idt_entry_int);
+	IDT_ENTRY(8, 0, idt_entry_int);
+	IDT_ENTRY(9, 0, idt_entry_int);
+	IDT_ENTRY(10, 0, idt_entry_int);
+	IDT_ENTRY(11, 0, idt_entry_int);
+	IDT_ENTRY(12, 0, idt_entry_int);
+	IDT_ENTRY(13, 0, idt_entry_int);
+	IDT_ENTRY(14, 0, idt_entry_int);
+	IDT_ENTRY(15, 0, idt_entry_int);
+	IDT_ENTRY(16, 0, idt_entry_int);
+	IDT_ENTRY(17, 0, idt_entry_int);
+	IDT_ENTRY(18, 0, idt_entry_int);
+	IDT_ENTRY(19, 0, idt_entry_int);
 	// llamar a IDT_ENTRY una vez por cada entrada a configurar (es una macro que no soporta loops)
 }
