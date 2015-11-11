fdb092c008a355db0f5d06475f6155d71115a36b
David <davidventura27@gmail.com>
2015-11-11 19:34:25 -0300

    instalar

--- documentacion/documentacion.sh
+++ documentacion/documentacion.sh
@@ -1,3 +1,24 @@
-#!/bin/sh
-egypt  --include-external {main,mmu,sched,tss,gdt}.c.192r.expand | dot -Grotate=90 -Gsize=50,45 -Tps -o kernel.ps
-egypt  --include-external {idt,isr_c,keyboard}.c.192r.expand | dot -Grotate=90 -Gsize=50,45 -Tps -o int.ps
+#!/bin/bash
+
+which dot >/dev/null
+if [ $? -ne 0 ]; then
+	echo "Instala graphviz"
+	exit 1
+fi
+which egypt >/dev/null
+if [ $? -ne 0 ]; then
+	cat << EOF
+wget http://www.gson.org/egypt/download/egypt-1.10.tar.gz
+tar xvf egypt*gz
+cd egypt*
+perl Makefile.PL
+make
+sudo make install
+EOF
+	exit 2
+fi
+egypt  --include-external {main,mmu,sched,tss,gdt}*.expand | dot -Grotate=90 -Gsize=50,45 -Tps -o kernel.ps
+egypt  --include-external {idt,isr_c,keyboard}*.expand | dot -Grotate=90 -Gsize=50,45 -Tps -o int.ps
+
+ls *ps
+echo "Abrilos con okular"
